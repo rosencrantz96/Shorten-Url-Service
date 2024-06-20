c25ba5069bf7bb2d95259f399b68f87897c921eb
권수경 <topgdvidsyb@naver.com>
2024-06-20 14:43:33 +0900

    테스트 코드 작성하기 - 컨트롤러에 대한 단위 테스트 추가

---
+++ src/test/java/kr/co/shortenurlservice/presentation/ShortenUrlRestControllerTest.java
@@ -0,0 +1,32 @@
+package kr.co.shortenurlservice.presentation;
+
+import kr.co.shortenurlservice.application.SimpleShortenUrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = ShortenUrlRestController.class)
+public class ShortenUrlRestControllerTest {
+
+    @MockBean
+    private SimpleShortenUrlService simpleShortenUrlService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    void redirectTest() throws Exception {
+        String expectedOriginalUrl = "https://www.hanbit.co.kr/";
+
+        when(simpleShortenUrlService.getOriginalUrlByShortenUrlKey(any())).thenReturn(expectedOriginalUrl);
+
+        mockMvc.perform(get("/any-key"))
+                .andExpect(status().isMovedPermanently())
+                .andExpect(header().string("Location", expectedOriginalUrl));
+    }
+}
